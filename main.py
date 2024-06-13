1d57e8101edb9cfe07d983b4747f976877fc4ecb
Brooke :) <133280277+nicolesgetaway@users.noreply.github.com>
2024-06-13 20:16:49 +0000

    Finished all Unit 2, Session 2 questions
    finished Problem Set A questions for Session 2, so all of Unit 2 is complete :)

--- main.py
+++ main.py
@@ -39,8 +39,7 @@ multiples_of_five()
 # Unit 2, Session 1, Problem Set A
 # Problem 1: Subsequences
 # This solution finds subsequences, but doesn't maintain relative order
-"""
-def is_subsequence(lst, sequence):
+"""def is_subsequence(lst, sequence):
   a = []
   for i in lst:
     for j in sequence:
@@ -52,8 +51,7 @@ def is_subsequence(lst, sequence):
     return "False"
 lst = [5, 1, 22, 25, 6, -1, 8, 10]
 sequence = [1, -1, 6, 10] # should be False, returns True
-print(is_subsequence(lst, sequence))
-"""
+print(is_subsequence(lst, sequence))"""
 
 # Corrected solution that satisfies all tests cases (duplicates and out-of-order)
 def is_subsequence(lst, sequence):
@@ -176,5 +174,123 @@ def index_to_value_map(lst):
 lst = ["apple", "banana", "cherry"]
 print(index_to_value_map(lst))
 
+# Unit 2, Session 2, Problem Set A
+# Problem 1: Cast Vote
+def cast_vote(votes, candidate):
+  if votes.get(candidate) is None:
+    votes[candidate] = 0      #added unregistered candidate to dict
+  for key, value in votes.items():
+    if key == candidate:
+      votes[candidate] = value + 1      #updates vote for desired candidate
+  return votes
+votes = {"Alice": 5, "Bob": 3}
+cast_vote(votes, "Alice")
+print(votes)
+cast_vote(votes, "Gina")
+print(votes)
 
+# Problem 2: Common Keys
+def common_keys(dict1, dict2):
+  a = []
+  for k in dict1:
+    if dict2.get(k) is not None:
+      a.append(k)
+  return a
+# Test 1 (Example Input Given)
+dict1 = {"a": 1, "b": 2, "c": 3}
+dict2 = {"b": 4, "c": 5, "d": 6}
+common_list = common_keys(dict1, dict2)
+print(common_list)
+# Test 2
+dict1 = {"a": 1, "b": 2, "c": 3, "d":1}
+dict2 = {"b": 4, "d": 5, "a": 6}
+common_list = common_keys(dict1, dict2)
+print(common_list)
+
+# Problem 3: Get Highest Priority
+def get_highest_priority_task(tasks):
+  highest = max(tasks.values())
+  priority = []
+  for key in tasks:
+    if tasks.get(key) == highest:
+      priority.append(key)
+  tasks.pop(priority[0])
+  return priority[0]  
+
+tasks = {"task1": 8, "task2": 10, "task3": 9, "task4": 10, "task5": 7}
+
+perform_task = (get_highest_priority_task(tasks)) # removes task2 bc first in alphabet
+print(perform_task)
+perform_task = (get_highest_priority_task(tasks)) # removes task4
+print(perform_task)
+perform_task = (get_highest_priority_task(tasks)) # removes task3
+print(perform_task)
+print(tasks) # task1, task5 should be left
+
+# Problem 4: Count Occurrences (Frequency Map)
+def count_occurrence(nums):
+  a = {}
+  for num in nums:
+    if num in a:  # frequency counter
+      a[num] += 1
+    else:   # creates new entry, prevents KeyError
+      a[num] = 1
+  return a
+
+#nums = [1, 2, 2, 3, 3, 3, 4]
+#print(count_occurrence(nums))
+# Problem 5: Majority Element (uses function from Problem 4)
+def find_majority_element(elements):
+  freq = count_occurrence(elements)
+  i = len(elements)
+  for key in freq:
+    if freq[key] > i/2:
+      return key
+    else:
+      return None
+elements = [2, 2, 1, 1, 1, 2, 2]
+print(find_majority_element(elements))  # should print '2'
+elements = [1, 1, 1, 1, 1, 2, 2]
+print(find_majority_element(elements)) # should print '1'
+elements = [1, 1, 1, 2, 2, 2]
+print(find_majority_element(elements)) #should print None
+
+# Problem 6: Find duplicates
+def hasDuplicates(nums, k):
+  if k > len(nums):
+    k = len(nums)
+  seen = []
+  for i in range(k):
+    #print(nums[i])
+    if nums[i] in seen:
+      return True
+    else:
+      seen.append(nums[i])
+  return False
+nums = [5, 6, 8, 2, 4, 6, 9]
+check1 = hasDuplicates(nums, 3)
+print(check1)
+check2 = hasDuplicates(nums, 4)
+print(check2)
+check3 = hasDuplicates(nums, 9)
+print(check3)
+
+# Problem 7: Make Pairs (uses Problem 4 function)
+def divideList(nums):
+  freq = count_occurrence(nums)
+  allEven = True
+  canDivide = None
+  for value in freq.values():
+    if value %2 != 0:
+      allEven = False
+  canDivide = bool(len(nums)%2 == 0 and allEven)
+  print(canDivide)
+nums = [3,2,3,2,2,2]
+divideList(nums) # should be True, pairs: (2,2) (2,2) (3,3)
+nums = [1,2,3,4]
+divideList(nums) # should be False
+nums = [5,6,5,6,5,6]
+divideList(nums) # should be False, uneven frequency
+nums = [6,5,6,5,6,5,5,6]
+divideList(nums) # should be True, pairs: (5,5) (5,5) (6,6) (6,6)
 
